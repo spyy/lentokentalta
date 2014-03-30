4d568bc9be19de1d61c9771401aa44f15f1ca358
spyy <sami.pyy@gmail.com>
2014-03-30 16:09:40 +0300

    Rikulle ja Jaakollee

--- index.php
+++ index.php
@@ -1,39 +1,27 @@
-<!--
-   untitled.html
-   
-   Copyright 2014 Sami Pyy <sami@spyy>
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-   MA 02110-1301, USA.
-   
-   
--->
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
+<!doctype html>
+<html lang="en">
 <head>
-	<title><?php echo "Lentokentalta.net";?></title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="Geany 1.22" />
+  <meta charset="utf-8">
+  <title><?php echo "Jaakon ja Rikun sivut";?></title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  #draggable { width: 200px; height: 200px; padding: 0.5em; }
+  </style>
+  <script>
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
+  </script>
 </head>
-
 <body>
-	
-	<h1>Jaakon sivut</h1>
+ 
+<div id="draggable" class="ui-widget-content">
+  <H1>Jaakon ja Rikun</H1>
+</div>
+ <h2>Sivut</h2>
+ 
 </body>
-
 </html>
